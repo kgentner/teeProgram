c93f4512cf58e8ba73d258f877c1b2d77de613f5
kgentner <gentnerkarl@gmail.com>
2014-11-03 11:12:05 -0800

    remove test file and clarify comment

--- tee.js
+++ tee.js
@@ -6,7 +6,7 @@ process.stdin.setEncoding('utf8');
 // pipe stdin to stdout
 process.stdin.pipe(process.stdout);
 
-//if a filename is included as arg in command line
+//if a file output is specified in command line
 //then also pipe stdin to a file
 //{'flags': 'a'} allows the file to be appended.
 if (filename){
