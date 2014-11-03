21c1e8763ab92016402b05618ec39caa9767a467
kgentner <gentnerkarl@gmail.com>
2014-11-03 11:09:18 -0800

    initial commit

---
+++ tee.js
@@ -0,0 +1,15 @@
+'use strict';
+var fs = require('fs');
+var filename = process.argv[2];
+
+process.stdin.setEncoding('utf8');
+// pipe stdin to stdout
+process.stdin.pipe(process.stdout);
+
+//if a filename is included as arg in command line
+//then also pipe stdin to a file
+//{'flags': 'a'} allows the file to be appended.
+if (filename){
+  process.stdin.pipe(fs.createWriteStream(filename, {'flags': 'a'}));
+}
+
