4e9593c4126f4722ff8a1dc25090d92f6da2cda8
kgentner <gentnerkarl@gmail.com>
2018-01-19 11:05:16 -0800

    updated tee.js again

--- tee.js
+++ tee.js
@@ -9,6 +9,7 @@ process.stdin.pipe(process.stdout);
 //if a file output is specified in command line
 //then also pipe stdin to a file
 //{'flags': 'a'} allows the file to be appended.
-if (filename) {
+if (filename)
+{
   process.stdin.pipe(fs.createWriteStream(filename, {'flags': 'a'}));
 }
